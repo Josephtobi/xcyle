389156b58b095846ce301d38715c8a5cf345831c
Joseph <joseph@MacBooks-MacBook-Pro.local>
2021-07-30 05:47:55 +0100

    cors header update

--- wastebackend/settings.py
+++ wastebackend/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_yc$y908r41s_z8mtbp%23j3i)#i+7&rhf#_2tc(@9wq638w@m'
-#os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
